dbc6e6c32ffb6cd524e3798a7ecbd46d41d57ff7
Favslight <samuelebiloma208@gmail.com>
2025-11-29 16:11:01 +0100

    push

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Import routes
 const authRoutes = require('./routes/authRoute');
-const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin/adminAuth');
 
 // Use routes
 app.use('/api/auth', authRoutes);
